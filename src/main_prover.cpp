9b467a9a4f3dff80067bcb6876adbd92d32b10fc
2xburnt <169301814+2xburnt@users.noreply.github.com>
2026-08-10 17:27:52 -0500

    Revert "Fix portable prover build"
    This reverts commit 8c38fc4ddc5e5867fc9e277570318121a31a6934.

--- src/main_prover.cpp
+++ src/main_prover.cpp
@@ -6,15 +6,43 @@
 #include "prover.h"
 #include "fileloader.hpp"
 
+#include <chrono>
+#include <gmp.h>
+#include <memory>
+#include <nlohmann/json.hpp>
+#include <alt_bn128.hpp>
+#include "binfile_utils.hpp"
+#include "zkey_utils.hpp"
+#include "wtns_utils.hpp"
+#include "groth16.hpp"
+
+using json = nlohmann::json;
+
+#define handle_error(msg) \
+           do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+using FpMilliseconds = std::chrono::duration<float, std::chrono::milliseconds::period>;
+#define START_TIMER(timer) auto timer##_start = std::chrono::high_resolution_clock::now();
+#define END_TIMER(timer, msg) printf("%s: %.0f ms\n", msg, FpMilliseconds(std::chrono::high_resolution_clock::now() - timer##_start).count());
+
 int main(int argc, char **argv)
 {
+    START_TIMER(prover_timer);
     if (argc != 5) {
         std::cerr << "Invalid number of parameters" << std::endl;
         std::cerr << "Usage: prover <circuit.zkey> <witness.wtns> <proof.json> <public.json>" << std::endl;
         return EXIT_FAILURE;
     }
 
+    START_TIMER(altBbn128r_timer);
+    mpz_t altBbn128r;
+
+    mpz_init(altBbn128r);
+    mpz_set_str(altBbn128r, "21888242871839275222246405745257275088548364400416034343698204186575808495617", 10);
+    END_TIMER(altBbn128r_timer, "init and set str for altBbn128r");
+
     try {
+        START_TIMER(get_zkey_wtns_timer);
         const std::string zkeyFilename = argv[1];
         const std::string wtnsFilename = argv[2];
         const std::string proofFilename = argv[3];
@@ -40,6 +68,8 @@ int main(int argc, char **argv)
         }
 
         groth16_proof_size(&proofSize);
+        auto zkey = BinFileUtils::openExisting(zkeyFilename, "zkey", 1);
+        auto zkeyHeader = ZKeyUtils::loadHeader(zkey.get());
 
         publicBuffer.resize(publicSize);
         proofBuffer.resize(proofSize);
@@ -60,16 +90,85 @@ int main(int argc, char **argv)
             throw std::runtime_error(errorMsg);
         }
 
-        std::ofstream proofFile(proofFilename);
-        proofFile.write(proofBuffer.data(), proofSize);
-
         std::ofstream publicFile(publicFilename);
         publicFile.write(publicBuffer.data(), publicSize);
 
+        std::string proofStr;
+        if (mpz_cmp(zkeyHeader->rPrime, altBbn128r) != 0) {
+            throw std::invalid_argument( "zkey curve not supported" );
+        }
+
+        auto wtns = BinFileUtils::openExisting(wtnsFilename, "wtns", 2);
+        auto wtnsHeader = WtnsUtils::loadHeader(wtns.get());
+
+        if (mpz_cmp(wtnsHeader->prime, altBbn128r) != 0) {
+            throw std::invalid_argument( "different wtns curve" );
+        }
+
+        END_TIMER(get_zkey_wtns_timer, "get zkey,zkeyHeader,wtns,wtnsHeader");
+
+        START_TIMER(make_prover_timer);
+        auto prover = Groth16::makeProver<AltBn128::Engine>(
+            zkeyHeader->nVars,
+            zkeyHeader->nPublic,
+            zkeyHeader->domainSize,
+            zkeyHeader->nCoefs,
+            zkeyHeader->vk_alpha1,
+            zkeyHeader->vk_beta1,
+            zkeyHeader->vk_beta2,
+            zkeyHeader->vk_delta1,
+            zkeyHeader->vk_delta2,
+            zkey->getSectionData(4),    // Coefs
+            zkey->getSectionData(5),    // pointsA
+            zkey->getSectionData(6),    // pointsB1
+            zkey->getSectionData(7),    // pointsB2
+            zkey->getSectionData(8),    // pointsC
+            zkey->getSectionData(9)     // pointsH1
+        );
+        END_TIMER(make_prover_timer, "make prover");
+
+        START_TIMER(wtnsData_timer);
+        AltBn128::FrElement *wtnsData = (AltBn128::FrElement *)wtns->getSectionData(2);
+        END_TIMER(wtnsData_timer, "get wtnsData");
+
+        START_TIMER(get_proof_timer);
+        auto proof = prover->prove(wtnsData);
+        END_TIMER(get_proof_timer, "generate proof");
+
+        START_TIMER(write_proof_to_file_timer);
+        std::ofstream proofFile;
+        proofFile.open (proofFilename);
+        proofFile << proof->toJson();
+        proofFile.close();
+        END_TIMER(write_proof_to_file_timer, "write proof to file");
+
+        START_TIMER(write_public_to_file_timer);
+        std::ofstream publicFile;
+        publicFile.open (publicFilename);
+
+        json jsonPublic;
+        AltBn128::FrElement aux;
+        for (int i=1; i<=zkeyHeader->nPublic; i++) {
+            AltBn128::Fr.toMontgomery(aux, wtnsData[i]);
+            jsonPublic.push_back(AltBn128::Fr.toString(aux));
+        }
+
+        publicFile << jsonPublic;
+        publicFile.close();
+        END_TIMER(write_public_to_file_timer, "write public to file");
+
+    } catch (std::exception* e) {
+        mpz_clear(altBbn128r);
+        std::cerr << "Error: " << e->what() << std::endl;
+        return EXIT_FAILURE;
     } catch (std::exception& e) {
+        mpz_clear(altBbn128r);
         std::cerr << "Error: " << e.what() << std::endl;
         return EXIT_FAILURE;
     }
+
+    mpz_clear(altBbn128r);
+    END_TIMER(prover_timer, "prover total");
 
     exit(EXIT_SUCCESS);
 }
